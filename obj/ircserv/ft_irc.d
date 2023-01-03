7e7764a189f908a36513e983fe1f3e459ed04749
Romain BRIARD <robriard@e1r4p3.clusters.42paris.fr>
2023-01-03 11:00:04 +0100

    add project name picture

--- obj/ircserv/ft_irc.d
+++
@@ -1,5 +0,0 @@
-obj/ircserv/ft_irc.o: src/ft_irc.cpp include/Server.class.hpp \
-  include/User.class.hpp include/Console.class.hpp \
-  include/string.operator.hpp include/type_traits.hpp \
-  include/Channel.class.hpp include/Error.hpp include/Response.hpp \
-  include/Utils.hpp include/Lexer.hpp include/Parser.hpp
